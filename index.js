8b611c0e793f03bee8f01326e035187f1fa03c3a
shivendra_pratap_dixit <cvendradixit@gmail.com>
2023-10-27 22:13:16 +0530

    Cors is added

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express=require("express");
 const { connection } = require("./db");
 const { userRouter } = require("./Routes/user.route");
 const { noteRouter } = require("./Routes/note.route");
+const cors=require("cors")
 const app=express();
+app.use(cors())
 app.use(express.json())
 app.use("/users",userRouter)
 app.use("/notes",noteRouter)
